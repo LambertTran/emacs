a0690948824b1413231b23182d415cfd633d4c7f
LBTran <lbtran@lbtran.com>
2022-01-10 09:12:26 -0800

    add dap mode

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
  ;; If there is more than one, they won't work right.
  '(doom-modeline-mode t)
  '(package-selected-packages
-   '(lsp-ui yasnippet lsp-mode go-autocomplete super-save powerline-evil disable-mouse visual-line-mode company-tern terraform-mode company-anaconda use-package)))
+   '(dap-go lsp-ui yasnippet lsp-mode go-autocomplete super-save powerline-evil disable-mouse visual-line-mode company-tern terraform-mode company-anaconda use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
